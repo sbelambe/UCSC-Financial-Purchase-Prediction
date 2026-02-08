6f26a74e1269260216f75dadead88c731ac60674
sbelambe <sbelambe@ucsc.edu>
2026-02-07 21:16:41 -0800

    initial set up of bucket

--- backend/app/firebase.py
+++ backend/app/firebase.py
@@ -1,6 +1,6 @@
 import os
 import firebase_admin
-from firebase_admin import credentials, firestore
+from firebase_admin import credentials, firestore, storage
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -19,9 +19,19 @@ cred_path = os.path.join(root_dir, cred_filename)
 if not firebase_admin._apps:
     if os.path.exists(cred_path):
         cred = credentials.Certificate(cred_path)
-        firebase_admin.initialize_app(cred)
+        bucket_name = os.getenv("FIREBASE_STORAGE_BUCKET")
+        if not bucket_name:
+            raise ValueError("FIREBASE_STORAGE_BUCKET is missing in .env")
+
+        firebase_admin.initialize_app(cred, {
+            "storageBucket": os.getenv("FIREBASE_STORAGE_BUCKET")
+        })
+
         print(f"Firebase initialized using key: {cred_filename}")
     else:
         raise FileNotFoundError(f"Could not find the key file at: {cred_path}")
 
+# initialize firestore client so we can write data to FireStore
 db = firestore.client()
+bucket = storage.bucket(bucket_name)
+
--- backend/data_cleaning/src/main.py
+++ backend/data_cleaning/src/main.py
@@ -2,6 +2,10 @@
 from clean_amazon import load_amazon
 from clean_cruzbuy import clean_cruzbuy
 # from .clean_pcard import clean_pcard
+from app.firebase import bucket
+import os
+
+
 
 def run_pipeline():
     """
@@ -24,5 +28,21 @@ def run_pipeline():
 
     return result
 
+def upload_csv_to_storage(local_path, storage_path):
+    """
+    Uploads a local CSV file to Firebase Storage.
+    """
+    blob = bucket.blob(storage_path)
+    blob.upload_from_filename(local_path, content_type="text/csv")
+    print(f"Uploaded {local_path} to Firebase Storage at {storage_path}")
+
+
 if __name__ == "__main__":
-    print(run_pipeline())
+    print(run_pipeline()) 
+
+    local_path = os.path.abspath(
+        os.path.join(os.path.dirname(__file__), "..", "clean", "cruzbuy_clean.csv")
+    )
+    storage_path = "clean/cruzbuy/cruzbuy_clean.csv"
+
+    upload_csv_to_storage(local_path, storage_path)
